6f7013418115187b7b5f4363eff2e980e73a11f7
unknown <dsjackowitz@gmail.com>
2023-07-03 11:02:33 -0400

    forage1

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+
+public class RewardValue {
+
+    double cashValue;
+    int milesValue;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+    }
+
+    public double getCashValue() {
+        return milesValue * .0035;
+    }
+
+    public int getMilesValue() {
+        return (int)(cashValue/.0035);
+    }
+}
